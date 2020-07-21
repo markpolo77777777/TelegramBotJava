980d6304dfa72d655b939dfbb6236622fc1bdf30
markpolo77777777 <marko.polo.2034@gmail.com>
2020-07-21 19:08:21 +0300

    Update Stickers.java

--- src/main/java/Stickers.java
+++ src/main/java/Stickers.java
@@ -115,9 +115,6 @@ public class Stickers extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "867439913:AAH9ZX7dC1FItEZI8DD3aAeN9MzlzQdhVW8";
+        return "token";
     }
 }
-
-//// ride it - regard
-/// !chatIDsURL.contains(update.getMessage().getChatId())
