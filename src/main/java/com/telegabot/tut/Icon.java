fcd81eb8f411a0acd1e5b889c448f5884ee76d81
marko <marko.polo.2034@gmail.com>
2020-07-22 17:17:35 +0300

    update

---
+++ src/main/java/com/telegabot/tut/Icon.java
@@ -0,0 +1,22 @@
+package com.telegabot.tut;
+
+import com.vdurmont.emoji.EmojiParser;
+
+public enum Icon {
+    PLUS(":heavy_plus_sign:"),
+    MINUS(":heavy_minus_sign:"),
+    CHECK(":white_check_mark:"),
+    NOT(":x:"),
+    DOUBT(":zzz:"),
+    FLAG(":checkered_flag:");
+
+    private String value;
+
+    public String get() {
+        return EmojiParser.parseToUnicode(value);
+    }
+
+    Icon(String value) {
+        this.value = value;
+    }
+    }
