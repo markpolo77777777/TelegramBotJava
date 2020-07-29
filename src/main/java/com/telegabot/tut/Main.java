cdae5c8ec5310ea97c0d99a8ef83fe63d65599ab
marko <marko.polo.2034@gmail.com>
2020-07-29 18:17:35 +0300

    update main

--- src/main/java/com/telegabot/tut/Main.java
+++ src/main/java/com/telegabot/tut/Main.java
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
+    //
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
